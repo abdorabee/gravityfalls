4c6551795eaaff9623efdc906937a1e49f3d94f3
abdorabee <abdorabee1134@gmail.com>
2025-01-26 20:38:43 +0200

    project setup

---
+++ src/sections/Header.tsx
@@ -0,0 +1,23 @@
+export const HeaderSection = () => {
+  return (
+    <header className="sticky top-0 bg-zinc-900/50 backdrop-blur-lg">
+      <div className="container">
+        <div className="flex justify-between items-center h-24">
+          <div>
+            <img src="/assets/images/logo.svg" alt="logo" />
+          </div>
+          <div>
+            <div className="size-10 relative">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
+              </div>
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
