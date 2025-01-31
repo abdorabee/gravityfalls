24b2a5831e4b5d8dda54237128fab0daa741ad70
abdorabee <abdorabee1134@gmail.com>
2025-02-01 01:39:43 +0200

    header setup

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,16 +2,35 @@ export const HeaderSection = () => {
   return (
     <header className="sticky top-0 bg-zinc-900/50 backdrop-blur-lg">
       <div className="container">
-        <div className="flex justify-between items-center h-24">
+        <div className="flex justify-between items-center h-24 md:h-28">
           <div>
             <img src="/assets/images/logo.svg" alt="logo" />
           </div>
           <div className="flex gap-4 items-center">
-            <button className="bg-fuchsia-500/20 px-4 py-2
-            font-extrabold uppercase font-heading text-sm tracking-wide relative">
-              <div className="absolute inset-0 outline outline-2
-              -outline-offset-2 outline-fuchsia-500"></div>
-              <span>Get Started</span>
+            <button
+              className="bg-fuchsia-500/20 px-4 py-2
+            font-extrabold uppercase font-heading text-sm tracking-wide relative hidden md:inline-flex"
+            >
+              <div
+                className="absolute inset-0 outline outline-2
+              -outline-offset-2 outline-fuchsia-500 [mask-image:linear"
+              ></div>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute top-0 right-0 text-fuchsia-500 [mask-image:linear-gradient(225deg,transparent,
+                transparent_10px,black_10px)]"
+              >
+                <path
+                  d="M0 1H12.2667L23 11.7333V24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                ></path>
+              </svg>
+              <span className="leading-6">Get Started</span>
             </button>
             <div className="size-10 relative">
               <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
