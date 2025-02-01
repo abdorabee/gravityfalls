8d23c92f3218e1d4bfe865645c4935c78c9b432a
abdorabee <abdorabee1134@gmail.com>
2025-02-01 20:10:35 +0200

    adding styles for the hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,13 +1,21 @@
+import { CutCornerButton } from "../components/CutCornerButton";
+
 export const HeroSection = () => {
   return (
-    <section>
+    <section className="py-24">
       <div className="container">
-        <p>Into the unknown</p>
-        <h1>The future of blockchain</h1>
-        <p>
+        <p className="uppercase font-extrabold text-center text-zinc-500 tracking-wider">
+          Into the unknown
+        </p>
+        <h1 className="font-heading font-black text-5xl text-center mt-4">
+          The Future of Blockchain is Here
+        </h1>
+        <p className="text-center text-xl mt-4 text-zinc-400">
           BlockForge is pioneering smart contract integrity with cutting-edge
           data solution.
         </p>
+        <CutCornerButton>Get Started</CutCornerButton>
+        <img src="/assets/images/icosahedron.png" alt="hero" />
       </div>
     </section>
   );
